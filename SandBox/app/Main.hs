0f72fb0cdd8c0313afcc9aa8287dcbdc67b11546
minato225 <1815131bill@gmail.com>
2022-09-20 18:24:42 +0300

    Guard

--- SandBox/app/Main.hs
+++ SandBox/app/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+analyzeGold :: Int -> String
+analyzeGold standard
+    | standard == 999 = "Wow! 999 standard!"
+    | standard == 750 = "Great! 750 standard."
+    | standard == 585 = "Not bad! 585 standard."
+    | otherwise = "I don't know such a standard..."
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn $ analyzeGold 999
